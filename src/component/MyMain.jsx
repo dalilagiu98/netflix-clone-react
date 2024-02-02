6d052a247be9f5ada7203b7a07bba48d07963975
dalilagiu98 <148708941+dalilagiu98@users.noreply.github.com>
2024-02-02 17:01:36 +0100

    padding

--- src/component/MyMain.jsx
+++ src/component/MyMain.jsx
@@ -4,15 +4,15 @@ const MyMain = function() {
     return(
         <div className="bg-dark text-white">
             <NavbarMain/>
-            <h3 className="fs-5 m-0 text-start mb-4 mt-3">Trending Now</h3>
+            <h3 className="fs-5 m-0 text-start ps-2 mb-4 mt-3">Trending Now</h3>
             <div>
                 <Galleries title="vrenuihvuerw"/>
             </div>
-            <h3 className="fs-5 m-0 text-start mb-4 mt-3">Watch It Again</h3>
+            <h3 className="fs-5 m-0 text-start ps-2 mb-4 mt-3">Watch It Again</h3>
             <div>
                 <Galleries title="one piece"/>
             </div>
-            <h3 className="fs-5 m-0 text-start mb-4 mt-3">New Release</h3>
+            <h3 className="fs-5 m-0 text-start ps-2  mb-4 mt-3">New Release</h3>
             <div>
                 <Galleries title="harry potter"/>
             </div>
